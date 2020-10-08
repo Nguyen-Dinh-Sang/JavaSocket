a9ff7eeea66c507d4de2d184b46059d94229abcf
DinhSang <dinhsangdroid@gmail.com>
2020-10-08 20:12:10 +0700

    Client-Sang: Fix connect

--- MusicInformationClient/src/musicinformationclient/socket/SocketClient.java
+++ MusicInformationClient/src/musicinformationclient/socket/SocketClient.java
@@ -1,16 +1,20 @@
-package musicinformationclient;
+package musicinformationclient.socket;
 
 import java.io.*;
 import java.net.Socket;
 
 public class SocketClient {
-    private Socket socket = null;
-    private BufferedWriter out = null;
-    private BufferedReader in = null;
+    private Socket socket;
+    private BufferedWriter out;
+    private BufferedReader in;
     private Result result;
+    private String address;
+    private int port;
 
     public SocketClient(String address, int port, Result result) {
         this.result = result;
+        this.address = address;
+        this.port = port;
         openSocket(address, port);
     }
 
@@ -25,45 +29,54 @@ public class SocketClient {
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (IOException ex) {
+            System.err.println("Lost connection");
+            System.out.println("Reconnection");
+            openSocket(address, port);
         }
     }
 
     public void closeSocket() {
+        System.out.println("Closing connection");
         try {
             in.close();
             out.close();
             socket.close();
-            System.err.println("Closing connection");
+            System.err.println("Connection closed");
+            result.closed();
         } catch (IOException ex) {
+
         }
     }
 
     public void send(String message) {
         try {
-            System.out.println("send: " + message);
+            System.out.println("Send: " + message);
             out.write(message);
             out.newLine();
             out.flush();
             listening();
         } catch (IOException ex) {
+            closeSocket();
         }
     }
 
-    private void listening(){
+    private void listening() {
         String line = null;
         try {
             line = in.readLine();
+            if (line != null) {
+                System.err.println("Result: " + line);
+                result.result(line);
+            }
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (line != null) {
-            System.err.println("Result: " + line);
-            result.result(line);
+            closeSocket();
         }
     }
 
     public interface Result {
 
         void result(String mes);
+
+        void closed();
     }
 }
